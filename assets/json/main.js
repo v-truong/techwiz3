614ae2571711b651a564901a37732885536114af
v-truong <truong.nv.1929@aptechlearning.edu.vn>
2022-08-11 15:25:36 +0700

    vantruong

--- assets/json/main.js
+++ assets/json/main.js
@@ -9,6 +9,10 @@ const app = {
       title: "Quần short nam",
       description: "aaaa",
       price: "$7",
+<<<<<<< Updated upstream
+=======
+      sex: "men",
+>>>>>>> Stashed changes
       product_code: "AB001",
     },
     {
@@ -17,6 +21,10 @@ const app = {
       title: "Sơ mi kẻ sọc nam",
       description: "cccc",
       price: "$20",
+<<<<<<< Updated upstream
+=======
+      sex: "men",
+>>>>>>> Stashed changes
       product_code: "AB002",
     },
     {
@@ -25,6 +33,10 @@ const app = {
       title: "chân váy nữ",
       description: "cccc",
       price: "$49",
+<<<<<<< Updated upstream
+=======
+      sex: "women",
+>>>>>>> Stashed changes
       product_code: "AB003",
     },
     {
@@ -33,14 +45,26 @@ const app = {
       title: "áo mua đông (nam/nữ)",
       description: "cccc",
       price: "$60",
+<<<<<<< Updated upstream
+=======
+      gender: 'unisex',
+>>>>>>> Stashed changes
       product_code: "AB004",
     },
     {
       id: 5,
+<<<<<<< Updated upstream
       images: "assets/img/SavedPictures/SavedPictures/anh005.jpg",
       title: "áo sơ mi cộc tay nữ",
       description: "cccc",
       price: "$21",
+=======
+      images: "assets/img/SavedPictures/SavedPictures/anh027.jpg",
+      title: "áo sơ mi cộc tay nữ",
+      description: "cccc",
+      price: "$21",
+      sex:"women",
+>>>>>>> Stashed changes
       product_code: "AB005",
     },
     {
@@ -49,6 +73,10 @@ const app = {
       title: "ÁO HAI DAY NỮ",
       description: "cccc",
       price: "$28",
+<<<<<<< Updated upstream
+=======
+      sex:"women",
+>>>>>>> Stashed changes
       product_code: "AB006",
     },
     {
@@ -57,6 +85,10 @@ const app = {
       title: "Quần bò nam",
       description: "cccc",
       price: "$98",
+<<<<<<< Updated upstream
+=======
+      sex:"men",
+>>>>>>> Stashed changes
       product_code: "AB007",
     },
     {
@@ -65,6 +97,10 @@ const app = {
       title: "áo sơ mi kẻ trắng nam",
       description: "cccc",
       price: "$48",
+<<<<<<< Updated upstream
+=======
+      sex:"men",
+>>>>>>> Stashed changes
       product_code: "AB008",
     },
     {
@@ -73,6 +109,10 @@ const app = {
       title: "quần bò đùi nam",
       description: "cccc",
       price: "$59",
+<<<<<<< Updated upstream
+=======
+      sex:"men",
+>>>>>>> Stashed changes
       product_code: "AB009",
     },
     {
@@ -81,6 +121,10 @@ const app = {
       title: "áo hodde đỏ (nam/nữ)",
       description: "cccc",
       price: "$80",
+<<<<<<< Updated upstream
+=======
+      gender: 'unisex',
+>>>>>>> Stashed changes
       product_code: "AB010",
     },
     {
@@ -89,6 +133,10 @@ const app = {
       title: "áo sơ mi cổ tay xọc đen nam",
       description: "cccc",
       price: "$96",
+<<<<<<< Updated upstream
+=======
+      sex:"men",
+>>>>>>> Stashed changes
       product_code: "AB011",
     },
     {
@@ -97,6 +145,10 @@ const app = {
       title: "áo ngắn nữ",
       description: "cccc",
       price: "$34",
+<<<<<<< Updated upstream
+=======
+      sex:"women",
+>>>>>>> Stashed changes
       product_code: "AB012",
     },
     {
@@ -105,6 +157,10 @@ const app = {
       title: "áo cộc 3 khuy nữ",
       description: "cccc",
       price: "$90",
+<<<<<<< Updated upstream
+=======
+      sex:"women",
+>>>>>>> Stashed changes
       product_code: "AB013",
     },
     {
@@ -113,6 +169,10 @@ const app = {
       title: "Quần bò nữ",
       description: "cccc",
       price: "$20",
+<<<<<<< Updated upstream
+=======
+      sex:"women",
+>>>>>>> Stashed changes
       product_code: "AB014",
     },
     {
@@ -121,6 +181,10 @@ const app = {
       title: "áo hồng kẻ nam",
       description: "cccc",
       price: "$65",
+<<<<<<< Updated upstream
+=======
+      sex:"men",
+>>>>>>> Stashed changes
       product_code: "AB015",
     },
     {
@@ -129,6 +193,10 @@ const app = {
       title: "quàn bò bó gối nam",
       description: "cccc",
       price: "$20",
+<<<<<<< Updated upstream
+=======
+      sex:"men",
+>>>>>>> Stashed changes
       product_code: "AB016",
     },
     {
@@ -137,6 +205,10 @@ const app = {
       title: "ao khoac dày nam",
       description: "cccc",
       price: "$98",
+<<<<<<< Updated upstream
+=======
+      sex:"men",
+>>>>>>> Stashed changes
       product_code: "AB017",
     },
     {
@@ -145,6 +217,10 @@ const app = {
       title: "áo guuci nam",
       description: "cccc",
       price: "$54",
+<<<<<<< Updated upstream
+=======
+      sex: "men",
+>>>>>>> Stashed changes
       product_code: "AB018",
     },
     {
@@ -153,6 +229,10 @@ const app = {
       title: "bộ thể thao nam",
       description: "cccc",
       price: "$12",
+<<<<<<< Updated upstream
+=======
+      sex: "men",
+>>>>>>> Stashed changes
       product_code: "AB019",
     },
     {
@@ -161,6 +241,10 @@ const app = {
       title: "áo ba lỗ adiads nam",
       description: "cccc",
       price: "$14",
+<<<<<<< Updated upstream
+=======
+      sex:"men",
+>>>>>>> Stashed changes
       product_code: "AB020",
     },
     {
@@ -169,6 +253,10 @@ const app = {
       title: "áo tua trăng nữ",
       description: "cccc",
       price: "$67",
+<<<<<<< Updated upstream
+=======
+      sex:"women",
+>>>>>>> Stashed changes
       product_code: "AB021",
     },
     {
@@ -177,6 +265,10 @@ const app = {
       title: "áo croptop nữ",
       description: "cccc",
       price: "$33",
+<<<<<<< Updated upstream
+=======
+      sex:"women",
+>>>>>>> Stashed changes
       product_code: "AB022",
     },
     {
@@ -185,6 +277,10 @@ const app = {
       title: "chân váy hoa nữ",
       description: "cccc",
       price: "$20",
+<<<<<<< Updated upstream
+=======
+      sex:"women",
+>>>>>>> Stashed changes
       product_code: "AB023",
     },
     {
@@ -193,6 +289,10 @@ const app = {
       title: "áo nen logan (nam/nữ)",
       description: "cccc",
       price: "$68",
+<<<<<<< Updated upstream
+=======
+      gender: 'unisex',
+>>>>>>> Stashed changes
       product_code: "AB024",
     },
     {
@@ -201,6 +301,10 @@ const app = {
       title: "Sơ mi kẻ sọc nam",
       description: "cccc",
       price: "$20",
+<<<<<<< Updated upstream
+=======
+      sex:"men",
+>>>>>>> Stashed changes
       product_code: "AB025",
     },
     {
@@ -209,6 +313,10 @@ const app = {
       title: "Sơ mi kẻ sọc nam",
       description: "cccc",
       price: "$20",
+<<<<<<< Updated upstream
+=======
+      sex:"men",
+>>>>>>> Stashed changes
       product_code: "AB026",
     },
     {
@@ -217,10 +325,15 @@ const app = {
       title: "Sơ mi kẻ sọc nam",
       description: "cccc",
       price: "$20",
+<<<<<<< Updated upstream
+=======
+      sex:"men",
+>>>>>>> Stashed changes
       product_code: "AB027",
     },
     {
       id: 28,
+<<<<<<< Updated upstream
       images: "assets/img/SavedPictures/SavedPictures/anh026.jpg",
       title: "Sơ mi kẻ sọc nam",
       description: "cccc",
@@ -250,6 +363,41 @@ const app = {
       description: "cccc",
       price: "$20",
       product_code: "AB02",
+=======
+      images: "assets/img/SavedPictures/SavedPictures/anh028.jpg",
+      title: "bộ sơ mi cộc tay trắng",
+      description: "cccc",
+      price: "$90",
+      gender: 'unisex',
+      product_code: "AB028",
+    },
+    {
+      id: 29,
+      images: "assets/img/SavedPictures/SavedPictures/anh029.jpg",
+      title: "Sơ mi kẻ sọc nam",
+      description: "cccc",
+      price: "$20",
+      sex:"men",
+      product_code: "AB029",
+    },
+    {
+      id: 30,
+      images: "assets/img/SavedPictures/SavedPictures/anh030.jpg",
+      title: "bộ quần áo nữ",
+      description: "cccc",
+      price: "$65",
+      sex:"women",
+      product_code: "AB030",
+    },
+    {
+      id: 31,
+      images: "assets/img/SavedPictures/SavedPictures/anh031.jpg",
+      title: "chân váy trắng nữ",
+      description: "cccc",
+      price: "$88",
+      sex:"women",
+      product_code: "AB031",
+>>>>>>> Stashed changes
     },
   ],
  
@@ -260,12 +408,12 @@ const app = {
             <div class="col-sm-3">
                 <div class="product">
                     <div class="product-img-info">
-                        <a href="" class="product__link">
-                            <img src="${product.images}" class="product__img" alt="">
+                        <!--a href="" class="product__link"-->
+                        <span data-target="#myModal"> <img  src="${product.images}" class="product__img" alt="" ></span>
                             <div class="product__sale">
                                 SALE!
                             </div>
-                        </a>
+                        <!--/a-->
                         <div class="item-box-hover">
                             <div class="cart-wrap">
                                 <i class="fas fa-shopping-cart"></i> ADD TO CART
@@ -303,6 +451,7 @@ const app = {
         `;
     });
     $(".product--container").innerHTML = htmls.join("");
+<<<<<<< Updated upstream
     // $('.product-index').innerHTML = htmls.join('');
     $('.product').innerHTML = htmls.join('');
   },
@@ -316,3 +465,18 @@ const app = {
 };
 
 app.start();
+=======
+    
+  
+  },
+
+  start: function () {
+      this.renderProduct();
+      this.showProductDetail();
+    
+  },
+};
+
+app.start();
+///truong code
+>>>>>>> Stashed changes
